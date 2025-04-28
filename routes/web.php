107e92f910907e1c228a6623a6dad7b1d5a3459f
Romain <mast3rsushi@gmail.com>
2025-04-28 11:34:56 +0200

    reorganisation des routes

--- routes/web.php
+++ routes/web.php
@@ -1,64 +1,57 @@
 <?php
+
 use App\Http\Controllers\ClientController;
 use App\Http\Controllers\MutuelleController;
-use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\AffichageDataController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
+// Page d'accueil
 Route::get('/', function () {
     return view('welcome');
 })->name('login');
 
+// 🔹 Routes générales Mutuelles
 Route::get('/mutuelles', [AffichageDataController::class, 'mutelles'])->name('mutuelles');
 
-Route::middleware('auth:mutuelles')
-    ->get('/mutuelle/home', [MutuelleController::class, 'homeView'])->name('mutuelle.home');
+// 🔹 Authentication Mutuelle
 Route::get('/mutuelle/login', [MutuelleController::class, 'loginView'])->name('mutuelles.login');
-
+Route::post('/mutuelle/login', [MutuelleController::class, 'login'])->name('mutuelle.login');
+Route::post('/mutuelle/register', [MutuelleController::class, 'register'])->name('mutuelle.register');
 Route::get('/mutuelle/logout', function () {
     Auth::guard('mutuelles')->logout();
-
     return redirect('/');
 })->name('mutuelle.logout');
-Route::get('/client/login', [ClientController::class, 'showLoginForm'])->name('client.login');
 
+// 🔹 Authentication Client
+Route::get('/client/login', [ClientController::class, 'showLoginForm'])->name('client.login');
+Route::post('/client/login', [ClientController::class, 'login'])->name('client.login');
+Route::post('/client/register', [ClientController::class, 'register'])->name('client.register');
 Route::get('/client/logout', function () {
     Auth::guard('clients')->logout();
-
     return redirect('/');
 })->name('client.logout');
 
-Route::middleware('auth:clients')
-    ->get('/client/home', [ClientController::class, 'homeView'])->name('client.home');
+// 🔹 Routes Mutuelle CRUD (Create / Read / Update / Delete)
 Route::get('/mutuelles/create', [MutuelleController::class, 'create'])->name('mutuelles.create');
-//Route::post('/mutuelles', [MutuelleController::class, 'store'])->name('mutuelles.store');
-Route::get('/mutuelles/{mutuelle}', [MutuelleController::class, 'show'])->name('mutuelles.show');
-
-//getting edit page route
-Route::get('/mutuelles/{mutuelle}/edit', [MutuelleController::class, 'edit'])->name('mutuelles.edit');
-//Route::put('/mutuelles/{mutuelle}', [MutuelleController::class, 'update'])->name('mutuelles.update');
-
-
-Route::post('/mutuelle/register', [MutuelleController::class, 'register'])->name('mutuelle.register');
-Route::post('/mutuelle/login', [MutuelleController::class, 'login'])->name('mutuelle.login');
-Route::post('/client/register', [ClientController::class, 'register'])->name('client.register');
 Route::post('/mutuelles', [MutuelleController::class, 'store'])->name('mutuelles.store');
-Route::post('/register', [ClientController::class, 'register'])->name('clients.store');
+
+Route::get('/mutuelles/{mutuelle}', [MutuelleController::class, 'show'])->name('mutuelles.show');
+Route::get('/mutuelles/{mutuelle}/edit', [MutuelleController::class, 'edit'])->name('mutuelles.edit');
+
+// Groupées sous auth:mutuelles
 Route::middleware('auth:mutuelles')->group(function () {
     Route::put('/mutuelles/{mutuelle}', [MutuelleController::class, 'update'])->name('mutuelles.update');
     Route::delete('/mutuelles/{mutuelle}', [MutuelleController::class, 'destroy'])->name('mutuelles.destroy');
-})->name('mutuelles.update');
 
-Route::post('/client/login', [ClientController::class, 'login'])->name('client.login');
-Route::middleware('auth:mutuelles')
-    ->get('/mutuelle/searchClient/{numero}', [MutuelleController::class, 'searchClientByNumeroSocial'])->name('mutuelle.searchClientByNumeroSocial');
-Route::get('/mutuelle/logout', [MutuelleController::class, 'logout'])->name('mutuelle.logout');
-
-Route::middleware(['auth:mutuelles'])->group(function () {
+    Route::get('/mutuelle/home', [MutuelleController::class, 'homeView'])->name('mutuelle.home');
+    Route::get('/mutuelle/searchClient/{numero}', [MutuelleController::class, 'searchClientByNumeroSocial'])->name('mutuelle.searchClientByNumeroSocial');
     Route::get('/mutuelle/clients', [MutuelleController::class, 'listeClients'])->name('mutuelle.clients');
 });
 
+// 🔹 Routes Client Dashboard (Protegées par auth:clients)
 Route::middleware('auth:clients')->group(function () {
+    Route::get('/client/home', [ClientController::class, 'homeView'])->name('client.home');
     Route::get('/client/edit', [ClientController::class, 'editProfile'])->name('client.editProfile');
     Route::put('/client/profile', [ClientController::class, 'updateProfile'])->name('client.updateProfile');
     Route::delete('/client/{id}', [ClientController::class, 'destroy'])->name('client.deleteProfile');
